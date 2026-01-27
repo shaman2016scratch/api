d1a2a9093c64a5442b5291854f77dcef669035bc
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-27 11:05:11 +0300

    Update index.js

--- api/tools/index.js
+++ api/tools/index.js
@@ -3,7 +3,25 @@ import fetch from 'node-fetch';
 export default async (req, res) => {
   const ouut = {
     "ok": true,
-    "result": {}
+    "result": {
+      "pagesCount": 3,
+      "pages": [
+        "tbc.html",
+        "mini-brow.html",
+        "search.html"
+      ],
+      "code": "https://github.com/shaman2016scratch/shaman2016scratch.github.io/tree/main/tools",
+      "version": {
+        "api": {
+          "main": "1.1.0"
+        },
+        "pages": {
+          "tbc": "2.1.0",
+          "mini-brow": "alpha",
+          "search": "alpha"
+        }
+      }
+    }
   }
   res.status(200).json(ouut)
 }
