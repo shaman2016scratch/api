4057a37f96af48e253498e80901e7690b6f2511f
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-27 20:23:30 +0300

    Update index.mjs

--- api/tools/index.mjs
+++ api/tools/index.mjs
@@ -1,6 +1,9 @@
 import fetch from 'node-fetch';
 
 export default async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   const ouut = {
     "ok": true,
     "result": {
