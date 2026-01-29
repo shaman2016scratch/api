bd5481ee89b1a5ec04fb6493b3359f23af21ac90
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-29 12:21:17 +0300

    Update index.js

--- api/tools/tbc/index.js
+++ api/tools/tbc/index.js
@@ -1,1 +1,31 @@
+import fetch from 'node-fetch';
 
+export default async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  const ouut = {
+    "ok": true,
+    "result": {
+      "pagesCount": 3,
+      "pages": [
+        "tbc.html",
+        "mini-brow.html",
+        "search.html"
+      ],
+      "code": "https://github.com/shaman2016scratch/shaman2016scratch.github.io/tree/main/tools",
+      "version": {
+        "api": {
+          "main": "1.1.0"
+        },
+        "pages": {
+          "tbc": "2.2.0",
+          "mini-brow": "alpha",
+          "search": "alpha"
+        }
+      }
+    },
+    "status": 200
+  }
+  res.status(200).json(ouut)
+}
