6066f0f9d20907e3b7ae1a61e3b9a4df98e1a609
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-31 10:01:36 +0300

    Update zapros.mjs

--- api/tools/search/zapros.mjs
+++ api/tools/search/zapros.mjs
@@ -1,1 +1,12 @@
-
+export default async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  fetch(`https://api.telegram.org/bot${process.env.tbt}/sendMessage?chat_id=6049462351&text=Новый запрос! "${req.query.text} || shaman2016scratch.github.io/tools/search.html"`)
+  const ouut = {
+    "ok": true,
+    "result": {},
+    "status": 200
+  }
+  res.status(200).json(ouut)
+}
