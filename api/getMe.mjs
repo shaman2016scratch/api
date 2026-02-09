c49db38b5c720b9009c8a624acfc85a89376e3b1
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-09 19:42:50 +0300

    Update getMe.mjs

--- api/getMe.mjs
+++ api/getMe.mjs
@@ -1,1 +1,15 @@
+import fetch from 'node-fetch';
 
+export default async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  const ouut = {
+    "ok": true,
+    "result": {
+      "user-agent": req.headers['user-agent'],
+      "ip": req.headers['x-forwarded-for']?.split(',')[0].trim()
+    }
+  }
+  res.status(200).json(ouut)
+}
