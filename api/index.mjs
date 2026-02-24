8a0cb86046d83532d10c035767754340bc58a6b2
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-24 22:21:06 +0300

    Update index.mjs

--- api/index.mjs
+++ api/index.mjs
@@ -6,7 +6,10 @@ export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   const ouut = {
     "ok": true,
-    "result": {}
+    "result": {
+      "description": "SHAMAN2016 API. Api from SHAMAN2016 on Node.js uploaded to Vercel",
+      "licence": "MIT License"
+    }
   }
   res.status(200).json(ouut)
 }
