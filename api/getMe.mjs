37cec5fdda24e6f3df20bacd3e4ba3933829b672
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-24 22:16:18 +0300

    Update getMe.mjs

--- api/getMe.mjs
+++ api/getMe.mjs
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIOS, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   const ouut = {
     "ok": true,
