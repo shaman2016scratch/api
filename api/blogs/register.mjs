0c83f9f65a019578c6ee78cf3ec729fa795edf63
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-03-31 22:32:54 +0300

    Update register.mjs

--- api/blogs/register.mjs
+++ api/blogs/register.mjs
@@ -1,1 +1,25 @@
+export default async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://shaman2016scratch.github.io, localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+  const { scratchUsername, name, password } = req.body
+  let comments = await fetch('https://api.scratch.mit.edu/users/shaman2016scratch/projects/1297964685/comments?limit=20')
+  comments = await comments.json()
+  const isVerified = comments.some(
+    (c) =>
+      c.author.username.toLowerCase() === scratchUsername.toLowerCase() &&
+      c.content.includes(decoded.code)
+  );
+  if (!isVerified) {
+    res.status(400).json({
+      'ok': false,
+      'error': 'not confirmed'
+    })
+  } else {
+    res.status(200).json({
+      'ok': true,
+      'result': 'Registration was successful. Now log in to your account'
+    })
+  }
+}
