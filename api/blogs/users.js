0831147d0d3fd8d214859c152040cc7f2788b66e
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-03-31 19:51:45 +0300

    Update users.js

--- api/blogs/users.js
+++ api/blogs/users.js
@@ -7,10 +7,21 @@
     ],
     'join': '2026-03-31T16:45:36.528Z',
     'isBanned': false,
-    'isDeleted': false
+    'isDeleted': false,
     'isAdmin': true,
     'isModer': true,
     'isDeveloper': true,
+    'type': 'public'
+  }, {
+    'name': 'DiamondCat2026',
+    'description': '',
+    'blogs': [],
+    'join': '2026-03-31T16:50:36.291Z',
+    'isBanned': false,
+    'isDeleted': false,
+    'isAdmin': false,
+    'isModer': false,
+    'isDeveloper': false,
     'type': 'public'
   }
 ]
