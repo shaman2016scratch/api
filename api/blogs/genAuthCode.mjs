827daf0534bae27daabaf99ea735c7d952e6f6cf
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-03-31 21:51:31 +0300

    Update genAuthCode.mjs

--- api/blogs/genAuthCode.mjs
+++ api/blogs/genAuthCode.mjs
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 const cryptoRandomString = require('crypto-random-string');
 
 export default async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://shaman2016scratch.github.io, localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
