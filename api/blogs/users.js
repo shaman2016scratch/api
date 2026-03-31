42da1a3c25508747bba10359d081f75a5029d321
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-03-31 19:48:55 +0300

    Update users.js

--- api/blogs/users.js
+++ api/blogs/users.js
@@ -5,6 +5,12 @@
     'blogs': [
       1
     ],
-    'join': '2026-03-31T16:45:36.528Z'
+    'join': '2026-03-31T16:45:36.528Z',
+    'isBanned': false,
+    'isDeleted': false
+    'isAdmin': true,
+    'isModer': true,
+    'isDeveloper': true,
+    'type': 'public'
   }
 ]
