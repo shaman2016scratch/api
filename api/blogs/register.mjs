cb0aca88037d30b074a774a8dd97504dc69f40e1
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-01 00:22:06 +0300

    Update register.mjs

--- api/blogs/register.mjs
+++ api/blogs/register.mjs
@@ -1,7 +1,15 @@
+import { Buffer } from 'buffer';
+
 export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://shaman2016scratch.github.io, localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  const owner = 'shaman2016scratch';
+  const repo = 'api';
+  const path = 'api/blogs/users.js';
+  const token = process.env.GITHUB_TOKEN;
+  const branch = 'main';
 
   const { scratchUsername, name } = req.body
   let comments = await fetch('https://api.scratch.mit.edu/users/shaman2016scratch/projects/1297964685/comments?limit=20')
@@ -32,9 +40,44 @@ export default async (req, res) => {
       'type': 'public',
       'scratch': scratchUsername
     })
-    res.status(200).json({
-      'ok': true,
-      'result': 'Registration was successful. Now log in to your account'
-    })
+    try {
+      const getFileResponse = await fetch(
+        `https://api.github.com/repos/${owner}/${repo}/contents/${filePath}?ref=${branch}`,
+        {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Accept': 'application/vnd.github.v3+json'
+          }
+        }
+      );
+
+      const fileData = await getFileResponse.json();
+      const currentSha = fileData.sha;
+      
+      const encodedContent = Buffer.from(usersDB).toString('base64');
+      await fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${filePath}`, {
+        method: 'PUT',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Accept': 'application/vnd.github.v3+json',
+          'Content-Type': 'application/json'
+        },
+        body: {
+          JSON.stringify({
+            message: `Update User List`,
+            content: encodedContent,
+            sha: currentSha,
+            branch: branch
+          })
+        }
+      })
+      res.status(200).json({
+        'ok': true,
+        'result': 'Registration was successful. Now log in to your account'
+      })
+    } catch (error) {
+      console.error(error)
+      res.status(500).json(error)
+    }
   }
 }
