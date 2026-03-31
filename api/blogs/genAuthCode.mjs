de0098d246ab9e842124d04d4cb5539f4acbe0e2
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-03-31 21:37:55 +0300

    Update genAuthCode.mjs

--- api/blogs/genAuthCode.mjs
+++ api/blogs/genAuthCode.mjs
@@ -1,16 +1,18 @@
 import fetch from 'node-fetch';
+const cryptoRandomString = require('crypto-random-string');
 
 export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  const code = cryptoRandomString({length: 25})
+  res.setHeader('Set-Cookie', `authCode=${code}; Path=/; HttpOnly; secure; sameSite=none; maxAge=300000`);
+
   const ouut = {
     "ok": true,
-    "result": {
-      "description": "SHAMAN2016 API. Api from SHAMAN2016 on Node.js uploaded to Vercel",
-      "licence": "MIT License",
-      "yourIp": req.headers["real-ip"]
-    }
+    "result": code
   }
   res.status(200).json(ouut)
 }
