1462cae366a3965a908068ee0a298274a4a56846
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-01 12:14:13 +0300

    Update login.mjs

--- api/blogs/login.mjs
+++ api/blogs/login.mjs
@@ -1,1 +1,7 @@
+import { Buffer } from 'buffer';
 
+export default async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://shaman2016scratch.github.io, localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+}
