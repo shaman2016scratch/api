5aa4a7c84449719fe56a2c340eba8a01b0f8aaed
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-01 12:15:57 +0300

    Update login.mjs

--- api/blogs/login.mjs
+++ api/blogs/login.mjs
@@ -1,4 +1,5 @@
 import { Buffer } from 'buffer';
+import cryptoRandomString from 'crypto-random-string';
 
 export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://shaman2016scratch.github.io, localhost:3000');
