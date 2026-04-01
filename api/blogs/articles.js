0594b8e6d5188b507219a8a5d1615e4bbb1043c0
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-01 13:15:30 +0300

    Update articles.js

--- api/blogs/articles.js
+++ api/blogs/articles.js
@@ -3,7 +3,7 @@
     'name': 'Блоги на сайте SHAMAN2016!',
     'description': '',
     'content': {},
-    'created': null,
-    'updated': null
+    'created': '2026-04-01T10:13:46.123Z',
+    'updated': '2026-04-01T10:13:46.123Z'
   }
 ]
