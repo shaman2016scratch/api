aec5f03f8eef46d8e38e93bf0ebd401d67541303
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-01 12:17:04 +0300

    Update register.mjs

--- api/blogs/register.mjs
+++ api/blogs/register.mjs
@@ -4,6 +4,7 @@ export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://shaman2016scratch.github.io, localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   const owner = 'shaman2016scratch';
   const repo = 'api';
