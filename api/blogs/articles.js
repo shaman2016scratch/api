46f660320afe9c213d0f7b1022ea4941e0aba306
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-01 13:19:14 +0300

    Update articles.js

--- api/blogs/articles.js
+++ api/blogs/articles.js
@@ -4,6 +4,10 @@
     'description': '',
     'content': {},
     'created': '2026-04-01T10:13:46.123Z',
-    'updated': '2026-04-01T10:13:46.123Z'
+    'updated': '2026-04-01T10:13:46.123Z',
+    'authors': [
+      { type: 'user', 'id': 1 },
+      { type: 'blog', 'id': 1 }
+    ]
   }
 ]
