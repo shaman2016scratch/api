be4441762cfb8e90471c639868c46a0b0ac23443
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-05-15 23:04:52 +0300

    Update data.js

--- api/scratchen/data.js
+++ api/scratchen/data.js
@@ -1,11 +1,14 @@
 const data = {
   users: {
     SHAMAN2016: 'AAA',
+    polzovatel_8787: 'AAA',
     Scratch_Lobo: 'AAB',
     DiamondCat2026: 'AAC',
+    'DiamondCat2026-2': 'AAC',
+    DiamondCatGroup: 'AAC',
     ilya6612288: 'AAD',
     qa3141: 'AAE',
-    Paps25: 'AAF',
+    PAPS25: 'AAF',
     EmerDog1234567: 'AAG',
     LonHaochen: 'ABA',
     mrmalettis_1234: 'ABB',
@@ -15,7 +18,19 @@ const data = {
     TheCloud_: 'ABF'
   },
   keys: {
-    AAA: { rep: 400 }
+    AAA: { rep: 400, nar: 0, tknHash: 'fbb8b2c628eda1c7928dc79733282684' },
+    AAB: { rep: 100, nar: 0, tknHash: 'b6d935223c3af6867a44d8f3c2032294' },
+    AAC: { rep: 210, nar: 0, tknHash: '05eb412c2ae2120c18b04d407d42ef68' },
+    AAD: {},
+    AAE: {},
+    AAF: {},
+    AAG: {},
+    ABA: {},
+    ABB: {},
+    ABC: {},
+    ABD: {},
+    ABE: {},
+    ABF: {}
   }
 }
 
