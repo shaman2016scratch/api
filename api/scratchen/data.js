ef4d09fb881f50104ccdd050011949401474779e
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-05-15 21:54:14 +0300

    Create data.js

---
+++ api/scratchen/data.js
@@ -0,0 +1,22 @@
+const data = {
+  users: {
+    SHAMAN2016: 'AAA',
+    Scratch_Lobo: 'AAB',
+    DiamondCat2026: 'AAC',
+    ilya6612288: 'AAD',
+    qa3141: 'AAE',
+    Paps25: 'AAF',
+    EmerDog1234567: 'AAG',
+    LonHaochen: 'ABA',
+    mrmalettis_1234: 'ABB',
+    BON_NAM_WAN_Dam: 'ABC',
+    Platon_BG: 'ABD',
+    MandosEshkerist: 'ABE',
+    TheCloud_: 'ABF'
+  },
+  keys: {
+    AAA: { rep: 400 }
+  }
+}
+
+export default data
