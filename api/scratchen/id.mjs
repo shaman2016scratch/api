8a087e7b78c5bb569a74e43a70471f1b5c0dd050
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-05-15 21:52:45 +0300

    Update id.mjs

--- api/scratchen/id.mjs
+++ api/scratchen/id.mjs
@@ -1,1 +1,26 @@
-
+export default async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  const data = {
+    users: {
+      SHAMAN2016: 'AAA',
+      Scratch_Lobo: 'AAB',
+      DiamondCat2026: 'AAC',
+      ilya6612288: 'AAD',
+      qa3141: 'AAE',
+      Paps25: 'AAF',
+      EmerDog1234567: 'AAG',
+      LonHaochen: 'ABA',
+      mrmalettis_1234: 'ABB',
+      BON_NAM_WAN_Dam: 'ABC',
+      Platon_BG: 'ABD',
+      MandosEshkerist: 'ABE',
+      TheCloud_: 'ABF'
+    },
+    keys: {
+      AAA: { rep: 400 }
+    }
+  }
+  res.status(200).json(ouut)
+}
